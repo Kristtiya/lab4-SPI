861e9f7cd24b80d83daa7d437ebdf6a525457a98
aditya-sudhakar <asudhakar@olin.edu>
2019-11-13 20:36:04 -0500

    pushing starter files

---
+++ spimemory.v
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------
+// SPI Memory
+//------------------------------------------------------------------------
+
+module spiMemory
+(
+    input           clk,        // FPGA clock
+    input           sclk_pin,   // SPI clock
+    input           cs_pin,     // SPI chip select
+    output          miso_pin,   // SPI master in slave out
+    input           mosi_pin,   // SPI master out slave in
+    output [3:0]    leds        // LEDs for debugging
+)
+
+
+endmodule
+   
