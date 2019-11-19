178b6692cebf1bd68032076b334ca3bdd1e5a369
Aditya <asudhakar@olin.edu>
2019-11-19 10:16:03 -0500

    updated data memory

--- modules/datamemory.v
+++ modules/datamemory.v
@@ -16,7 +16,8 @@ module datamemory
     output reg [width-1:0]      dataOut,
     input [addresswidth-1:0]    address,
     input                       writeEnable,
-    input [width-1:0]           dataIn
+    input [width-1:0]           dataIn,
+    input                       reset
 );
 
     reg [width-1:0] memory [depth-1:0];
@@ -26,5 +27,21 @@ module datamemory
             memory[address] <= dataIn;
             dataOut <= memory[address];
     end
+
+
+    //SO I've read that FPGAs have no known value for any registers
+    //on startup so it's common practice to set a reset line to necessary
+    //modules in order to set them to a known value -> Stack Overflow
+    reg[31:0] k;
+    always@(posedge reset)begin
+      if (reset) begin
+        for (k =0 ;k<3'b111 ;k = k + 1 ) begin
+          memory[k] = 8'b0;
+          end
+          // memory[7] = 8'b101010; //points to the begening of the stack
+          //                      //source -> Mars
+      end
+    end
+
 
 endmodule
