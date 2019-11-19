e155fcd8309c2f095a3ea70e0673b59ea20d5e0e
Aditya <asudhakar@olin.edu>
2019-11-19 13:58:58 -0500

    updating datamemory

--- modules/inputconditioner.t.v
+++ modules/inputconditioner.t.v
@@ -1,7 +1,9 @@
+timescale 1ns / 1ps
+
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
-`include "inputconditioner.v" 
+`include "inputconditioner.v"
 
 module testConditioner();
 
@@ -10,7 +12,7 @@ module testConditioner();
     wire conditioned;
     wire rising;
     wire falling;
-    
+
     inputconditioner dut(.clk(clk),
     			         .noisysignal(pin),
 			             .conditioned(conditioned),
@@ -19,70 +21,70 @@ module testConditioner();
     // Generate clock (50MHz)
     initial clk = 1;
     always #10 clk=!clk;    // 50MHz Clock
-    
+
     initial begin
     $dumpfile("inputconditioner.vcd");
         $dumpvars(0, dut);
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
-    repeat(20) begin 
+    repeat(20) begin
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
     end
-    repeat(5) begin 
+    repeat(5) begin
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin   //Ensure super noisy singal does not send posedge or negedge signals 
+    repeat(10) begin   //Ensure super noisy singal does not send posedge or negedge signals
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
         pin = 0;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(5) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs 
+    repeat(5) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(4) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs 
+    repeat(4) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(3) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs 
+    repeat(3) begin  //Short Signal _too short for wait-time so should not send posedge or negedge outputs
         pin = 1;
 	    $display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
 
-    repeat(10) begin 
+    repeat(10) begin
     pin = 0;
 	$display("%t | %d | %d | %d | %d | %d |", $time, clk, pin, conditioned, rising, falling); #20;
 	end
@@ -90,5 +92,5 @@ module testConditioner();
 	$display("... more execution (see waveform)");
 		$finish();
 	end
-    
+
 endmodule
